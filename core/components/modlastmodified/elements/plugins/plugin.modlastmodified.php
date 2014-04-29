966d111117acfec6b0e08c10961878afc630131b
ilyaut <ilyaut@webkrl.beget.ru>
2014-04-29 15:56:28 +0400

    0.0.4

--- core/components/modlastmodified/elements/plugins/plugin.modlastmodified.php
+++ core/components/modlastmodified/elements/plugins/plugin.modlastmodified.php
@@ -8,22 +8,20 @@ switch ($modx->event->name) {
 		break;
         
     case 'OnWebPagePrerender':
+        $modified = date("r", strtotime($modx->resource->get('editedon')));
+        header ("Last-Modified: $modified");
+        
         $modx->resource->_contextKey = $modx->resource->context_key;
         $cache = $modx->cacheManager->getCacheProvider($modx->getOption('cache_resource_key', null, 'resource'));
         $key = $modx->resource->getCacheKey();
         if ($cache->get($key)) {
             $qtime = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : '';
-            $modified = date("r", strtotime($modx->resource->get('editedon')));
-            if ($qtime == $modified) {
+            if (strtotime($qtime) >= strtotime($modified)) {
                 header ("HTTP/1.1 304 Not Modified ");
-                header ("Last-Modified: $modified");
-                exit();
-            } else {
-                header ("Last-Modified: $modified");
                 header("Expires: " . date("r", time() + 3600));
+                exit();
             }
-        } else {
-            header("Expires: " . date("r", time() + 3600));
         }
+        header("Expires: " . date("r", time() + 3600));
         break;
 }
